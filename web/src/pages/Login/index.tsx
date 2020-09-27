0498fda2c25e499459b1189dd71fb19b0406bc58
Vinícius Gomes <vinigc19@gmail.com>
2020-09-27 08:06:47 -0300

    Modelo página de login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 import Logo from '../../assets/doggo.jpg';
 
+import Input from "../../components/Input";
+
+import './styles.css';
+
 function Login() {
     return (
-        <img src={Logo} alt="Doggo"/>
+        <div className="container">
+            <div className="image-container">
+                <h1 className="text">Doggo</h1>
+                <h6>Não deixe que os preceitos te definam.</h6>
+                
+                <img src={Logo} alt="Doggo" />
+            </div>
+            <form>
+                <h1 className="text">Doggo</h1>
+
+                <Input label="Usuário" name="user" auxText="Insira seu usuário"/>
+                <Input label="Senha" name="password" auxText="Insira sua senha" />
+            </form>
+
+        </div>
     );
 };
 
