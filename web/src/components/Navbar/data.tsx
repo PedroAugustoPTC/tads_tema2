ccee60a1d03580052516ed5bed77e5656b19ecc7
Vinícius Gomes <vinigc19@gmail.com>
2020-10-29 09:53:38 -0300

    Padronizando LoginPage com Figma

---
+++ web/src/components/Navbar/data.tsx
@@ -0,0 +1,23 @@
+export const Data = [
+    {
+        title: 'Perfil',
+        path: "/Home",
+        icon: "Icon1",
+        cName: 'nav-text'
+    },
+    {
+        title: 'Produtos',
+        path: "/Products",
+        icon: "Icon2",
+        cName: 'nav-text'
+    },
+    {
+        title: 'Reports',
+        path: "/Reports",
+        icon: "Icon3",
+        cName: 'nav-text'
+    }
+
+]
+
+export default Data;
